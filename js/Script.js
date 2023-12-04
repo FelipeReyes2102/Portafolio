bc2c8f0652e5850429b3ad97d7d47a5b027d43b4
FelipeReyes2102 <felipereyesreyes2102@gmail.com>
2023-12-03 22:36:11 -0600

    Terminado

--- js/Script.js
+++ js/Script.js
@@ -62,3 +62,58 @@ circles.forEach(elem=>{
 
 
 var mixer = mixitup('.portafolio-gallery');
+
+//Active Menu
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+    let len = section.length;
+    while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active"); 
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 50)
+})
+
+// Toggle icon navbar///////////////////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x");
+    navList.classList.toggle("open");
+}
+
+window.onscroll = () =>{
+    menuIcon.classList.remove("bx-x");
+    navList.classList.remove("open");
+}
+
+// Toggle icon navbar///////////////////////////////////
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-items");
+        } else {
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollScale.forEach((el) => observer.observe(el));
